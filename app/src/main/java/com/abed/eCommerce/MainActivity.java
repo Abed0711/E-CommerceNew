0c34a1464ef0512f408dbec155f72064fa9aa3db
Md. Abedur Rahman <abed.baust@gmail.com>
2021-05-11 15:28:57 +0600

    Reg Page done

--- app/src/main/java/com/abed/eCommerce/MainActivity.java
+++ app/src/main/java/com/abed/eCommerce/MainActivity.java
@@ -2,13 +2,31 @@ package com.abed.eCommerce;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView joinNowTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        joinNowTextView = findViewById(R.id.gotoReg);
+
+        joinNowTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,regestrationActivity.class);
+                startActivity(intent);
+            }
+        });
+        
+
     }
 }
